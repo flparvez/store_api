f45971d248bd84f124c152a4f745b1650adcc6b9
flparvez <flparvez23@gmail.com>
2024-11-23 16:37:01 +0600

    add slug in category

--- src/models/category.models.js
+++ src/models/category.models.js
@@ -5,4 +5,23 @@ const categorySchema= new mongoose.Schema({
     title: { type: String, required: true, unique: true },
     slug: { type: String, unique: true },
 },{timestamps: true});
+
+
+
+
+// Pre-save hook to generate slug from name
+categorySchema.pre('save', async function(next) {
+    if (this.isModified('title')) {
+      this.slug = slugify(this.title, { lower: true, strict: true });
+  
+      // Ensure slug is unique
+      const slugExists = await mongoose.models.Category.findOne({ slug: this.slug });
+      if (slugExists) {
+        this.slug = `${this.slug}-${Date.now()}`;
+      }
+    }
+    next();
+  });
+  
+
 export const Category = mongoose.model('Category', categorySchema);
